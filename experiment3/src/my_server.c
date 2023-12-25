4383d19c6ec36cf4fcebdb3cad61d6f9cb18257b
GrassedgeT <grassedge@outlook.com>
2023-12-25 19:41:04 +0800

    新建

---
+++ experiment3/src/my_server.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/epoll.h>
+#include <fcntl.h>
+
+#define MAX_EVENTS 10
+#define PORT 8080
+#define BUFFER_SIZE 1024
