84f13751efe2dbcbc0144a58eec0a5270c892bc8
qianniancao <1962400688@qq.com>
2023-12-25 14:21:18 +0800

    myls-R完成

--- experiment2/src/myls-R.c
+++ experiment2/src/myls-R.c
@@ -21,8 +21,12 @@ void list_dir(const char *dir_name) {
         perror("opendir");
         return;
     }
+    if (chdir(dir_name) != 0) {
+        perror("chdir");
+        return 1;
+    }
 
-    //打印表头
+    print_cwd();
     printf("%-25s %10s\t%s\n", "文件名", "大小", "最后修改时间");  
 
     struct dirent *entry;
@@ -31,32 +35,48 @@ void list_dir(const char *dir_name) {
     }
 
     closedir(dir);
+    if (chdir("..") != 0) {
+        perror("chdir");
+        return 1;
+    }
 }
 
 void list_dir_recursive(const char *dir_name){
+    
+    list_dir(dir_name);
     DIR *dir = opendir(dir_name);
     if (dir == NULL) {
         perror("opendir");
         return;
     }
-
-
-    printf("%s:\n", dir_name);
-    printf("%-25s %10s %s\n", "文件名", "大小", "最后修改时间");
-
+    if (chdir(dir_name) != 0) {
+        perror("chdir");
+        return 1;
+    }
     struct dirent *entry;
     while ((entry = readdir(dir)) != NULL) {
-        print_file_info(entry->d_name);
-
-
+    
         if (entry->d_type == DT_DIR && strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
-            char sub_dir_name[PATH_MAX];
-            snprintf(sub_dir_name, sizeof(sub_dir_name), "%s/%s", dir_name, entry->d_name);
-            list_dir_recursive(sub_dir_name);
+            list_dir_recursive(entry->d_name);
         }
     }
 
     closedir(dir);
+    if (chdir("..") != 0) {
+        perror("chdir");
+        return 1;
+    }
+}
+
+void print_cwd(){
+    char cwd[PATH_MAX];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("\n%s\n", cwd);
+    } else {
+        perror("getcwd");
+        return 1;
+    }
+
 }
 
 int main(int argc, char *argv[]) {
@@ -67,28 +87,29 @@ int main(int argc, char *argv[]) {
             if (argc == 2) {
                 list_dir(".");
             } else {
-                if (chdir(argv[2]) != 0) {
-                    perror("chdir");
-                    return 1;
-                }
-                list_dir(".");
+                // if (chdir(argv[2]) != 0) {
+                //     perror("chdir");
+                //     return 1;
+                // }
+                
+                list_dir(argv[2]);
             }
         } else if (strcmp(argv[1], "-R") == 0) {
             if (argc == 2) {
                 list_dir_recursive(".");
             } else {
-                if (chdir(argv[2]) != 0) {
-                    perror("chdir");
-                    return 1;
-                }
-                list_dir_recursive(".");
+                // if (chdir(argv[2]) != 0) {
+                //     perror("chdir");
+                //     return 1;
+                // }
+                list_dir_recursive(argv[2]);
             }
         } else {
-            if (chdir(argv[1]) != 0) {
-                perror("chdir");
-                return 1;
-            }
-            list_dir(".");
+            // if (chdir(argv[1]) != 0) {
+            //     perror("chdir");
+            //     return 1;
+            // }
+            list_dir(argv[1]);
         }
     }
 
