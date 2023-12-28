276483cdd0bed4b8ef5c5359148f86350b4bab69
GrassedgeT <grassedge@outlook.com>
2023-12-28 13:49:36 +0800

    跳转至room_list界面完成

--- final_project/src/client.c
+++ final_project/src/client.c
@@ -1,10 +1,27 @@
 #include <ncurses.h>
 #include <locale.h>
+#include <asm/signal.h>
 
-int main(int argc,char* argv[]){
-    setlocale(LC_ALL, ""); // 设置locale以支持中文
-    initscr();
-    start_color();
+void handle_winch(int sig){
+
+    endwin();
+    clear();
+    draw_title();
+    refresh();
+    // 你可能还需要重新绘制你的界面
+}
+
+void room_list() {
+    WINDOW *win = newwin(0, 0, 0, 0); // 创建一个新窗口，其大小与终端窗口相同
+    box(win, 0, 0);
+    wrefresh(win);
+    // 在这个窗口中做一些事情
+    wgetch(win);
+    delwin(win);
+}
+
+void draw_title(){
+
     init_pair(1, COLOR_RED, COLOR_BLACK);
     init_pair(2, COLOR_GREEN, COLOR_BLACK);
     init_pair(3, COLOR_YELLOW, COLOR_BLACK);
@@ -12,10 +29,6 @@ int main(int argc,char* argv[]){
     init_pair(5, COLOR_MAGENTA, COLOR_BLACK);
     init_pair(6, COLOR_CYAN, COLOR_BLACK);
     init_pair(7, COLOR_WHITE, COLOR_BLACK);
-
-    raw();
-    noecho();
-    curs_set(0);
 
     char* title[] = {
         "  GGGG  RRRRR   AAA  SSSSS SSSSS EEEEE DDDDD  GGGG  EEEEE ",
@@ -42,8 +55,26 @@ int main(int argc,char* argv[]){
     char* start_prompt = "  按下任意键开始游戏";
     mvwprintw(stdscr, LINES - 10, (COLS - strlen(start_prompt)) / 2 + 5, "%s", start_prompt);
     
+}
+
+int main(int argc,char* argv[]){
+    setlocale(LC_ALL, ""); // 设置locale以支持中文
+    initscr();
+    start_color();
+   
+
+    signal(SIGWINCH, handle_winch); 
+
+    raw();
+    noecho();
+    curs_set(0);
+
+    draw_title();
+    
     refresh();
     getch();
+    signal(SIGWINCH, room_list); 
+    room_list();
     endwin();
 
     return 0;
