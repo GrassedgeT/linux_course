52f3fc7c68b4ade5190348667714a6685949f415
GrassedgeT <grassedge@outlook.com>
2023-12-28 20:27:58 +0800

    添加生成指定范围随机数函数

--- final_project/src/lib/utils.h
+++ final_project/src/lib/utils.h
@@ -4,11 +4,10 @@
 #include <time.h>
 
 //添加随机后缀的函数
-char* add_random_suffix(const char* str) {
-    srand(time(NULL)); // 初始化随机数生成器
-    int random_number = rand() % 10000; // 生成一个四位随机数
+char* add_random_suffix(const char* str, int random_number) {
 
     char* new_str = (char*)malloc(strlen(str) + 5); // 分配足够的内存来存储新的字符串
+    
     if (new_str == NULL) {
         return NULL; // 内存分配失败
     }
@@ -16,4 +15,10 @@ char* add_random_suffix(const char* str) {
     sprintf(new_str, "%s%04d", str, random_number); // 格式化新的字符串
 
     return new_str;
+}
+
+//生成指定范围随即整数的函数
+int random_int(int min, int max) {
+    srand(time(NULL)); // 初始化随机数生成器
+    return rand() % (max - min + 1) + min; // 生成一个指定范围的随机数
 }
