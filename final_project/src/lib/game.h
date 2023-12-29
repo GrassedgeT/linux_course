078fdc9131fbbcd42dabced051a5aae07319514c
GrassedgeT <grassedge@outlook.com>
2023-12-29 20:28:38 +0800

    优化数据结构

--- final_project/src/lib/game.h
+++ final_project/src/lib/game.h
@@ -1,32 +1,34 @@
 //存放有关有关游戏的数据和操作
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 #define MAX_HP 150
 #define MAX_ATK 50
 #define MAX_ATK_RANGE 10
 #define MAX_LEVEL 20
-#define MAX_PLAYER_NUM 20
+#define MAX_PLAYER_NUM 10
 
 typedef struct Player{
-    int id;
+    uint16_t id;
     char* name;
-    int Hp; //生命值
-    int Atk; //攻击力
-    int Atk_range; //攻击范围
-    int level; //等级
-    int score; //分数
-    int exp; //经验值
-    int next_level_exp; //升级所需经验值
+    uint8_t Hp; //生命值
+    uint8_t Atk; //攻击力
+    uint8_t Atk_range; //攻击范围
+    uint8_t level; //等级
+    uint8_t score; //分数
+    uint8_t exp; //经验值
+    uint8_t next_level_exp; //升级所需经验值
     int x; //横坐标
     int y; //纵坐标
-    int status; //状态0:死亡 1：正常 2：无敌
+    uint8_t status; //状态0:死亡 1：正常 2：无敌
 }Player;
 
 typedef struct Room
 {
+    uint16_t id;
     char* name;
-    int player_num;
+    uint8_t player_num;
     struct Player* players;
 }Room;
 
@@ -35,11 +37,11 @@ typedef struct Room
 //小怪
 typedef struct Monster{
     char* name;
-    int Hp; //生命值
-    int Atk; //攻击力
+    uint8_t Hp; //生命值
+    uint8_t Atk; //攻击力
     int x; //横坐标
     int y; //纵坐标
-    int status; //状态0:死亡 1：正常
+    u_int8_t status; //状态0:死亡 1：正常
 }Monster;
 
 typedef struct Msg{
